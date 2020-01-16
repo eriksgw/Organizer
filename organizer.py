e8fcc435139830f0ed9e2153b11350b601b487f3
ErikSugawara <42067201+ErikSugawara@users.noreply.github.com>
2020-01-16 13:17:16 -0300

    Added paths

--- organizer.py
+++ organizer.py
@@ -4,14 +4,17 @@ import sys
 import shutil
 
 def moveFileToDest(ext, sourcePath, destinyPath):
-    for files in os.listdir("./Downloads"):
+    for files in os.listdir(sourcePath):
+        sourcePathFile = sourcePath+'/'+files
         if ext in files:
-            shutil.move("./Downloads/"+files, "./Downloads/Organizer/"+ext+"/"+files)
-            print(files+"moved")
-def getAllExt():
+            destinyPathFile = destinyPath+'/'+files
+            shutil.move(sourcePathFile, destinyPathFile)
+            print(files+" moved")
+
+def getAllExt(sourcePath):
 
     ext_list = []
-    for files in os.listdir("./Downloads"):
+    for files in os.listdir(sourcePath):
         extension = os.path.splitext(files)[1]
         if not extension in ext_list:
             if not extension == '':
@@ -21,24 +24,24 @@ def getAllExt():
 
 
 def main():
-    if not os.path.exists("./Downloads/Organizer"):
-        os.mkdir('./Downloads/Organizer')
+    directoryPath = input('Insert your directory path:')
+    directoryOrganizer = directoryPath+'/Organizer'
+    if not os.path.exists(directoryPath):
+        os.mkdir(directoryOrganizer)
         print("Organizer folder created")
 
     else:
         print("Starting...")
-        extension_list = getAllExt()
-        print(extension_list)
+        extension_list = getAllExt(directoryPath)
         for ext in extension_list:
-            print("./Downloads/Organizer/"+ext)
-            if not os.path.exists("./Downloads/Organizer/"+ext):
-                os.mkdir("./Downloads/Organizer/"+ext)
-                print("./Downloads/Organizer/"+ext)
+            directoryOrganizerExt = directoryOrganizer+'/'+ext
+            if not os.path.exists(directoryOrganizerExt):
+                os.mkdir(directoryOrganizerExt)
             else:
                 print(ext+"folder already created")
 
         for ext in extension_list:
-           moveFileToDest(ext,"./Downloads","./Downloads/Organizer/"+ext)
+           moveFileToDest(ext,directoryPath,directoryOrganizerExt)
 
 
 main()
